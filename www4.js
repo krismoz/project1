9680a28d642d8deddc519e2a1cc4bcb7a5b63d55
Kris Mozgovoi <krismoz@users.noreply.github.com>
2026-09-23 15:25:59 +0300

    Esimesed marsruudid ja esimene async funktsioon

--- www4.js
+++ www4.js
@@ -1,17 +1,67 @@
 const http = require('http');
+//moodul URL päringu parsimiseks
+const url = require('url');
+//moodul failitee haldamiseks
+const path = require('path');
+//const fs = require('fs');
+const fs = require('fs').promises;
 const dateTimeET = require('./src/dateTimeET.js');
 const pageHead = '<!DOCTYPE html>\n<html lang="et">\n<head>\n\t<meta charset="utf-8">\n\t<title>Kris Mozgovoi | Veebiprogrammeerimine</title>\n</head>\n<body>\n';
-const pageBody = '\t<h1>Kris Mozgovoi | Veebiprogrammeerimine</h1>\n\t <p>See leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee">Tallinna Ülikoolis</a> ning ei sisalda tõsiseltvõetavat sisu!</p>\n\t<p>Esialgu tutvusime lihtsalt HTML keelega, peatselt programmeerime.</p>\n\t<hr>\n';
+const pageBanner = '\t<img src="veebiprogrammeerimine_2026_AA.png" alt="banner">';
+const pageBody = '\t<h1>Kris Mozgovoi | Veebiprogrammeerimine</h1>\n\t<p>See leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee">Tallinna Ülikoolis</a> ning ei sisalda tõsiseltvõetavat sisu!</p>\n\t<p>Esialgu tutvusime lihtsalt HTML keelega, peatselt programmeerime.</p>\n\t<hr>\n';
 const pageFoot = '\n</body>\n</html>';
 
-http.createServer(function(req, res){
-	res.writeHead(200, {"Content-type": "text/html"});
-	//res.write('Meie veeb käivitus!');
-	res.write(pageHead);
-	res.write(pageBody);
-	// Leidsin lahenduse mis ei toiminud mul kuni ma asendasin ' märgid ära ` märkidega chatgpt abiga. Kuid me ei ole veel sellest õppinud seega ma ei kasutanud seda.
-	//res.write(`\t<p>Hetkel on ${dateTimeET.dayET()}.</p>\n\t<p>Kuupäev on: ${dateTimeET.dateET()}</p>\n\t<p>Lehekülg avati kell: ${dateTimeET.timeET()}</p>`);
-	res.write('\t<p>Hetkel on ' + dateTimeET.dayET() + '.</p>\n\t<p>Kuupäev on: ' + dateTimeET.dateET() + '</p>\n\t<p>Lehekülg avati kell: ' + dateTimeET.timeET() + '</p>');
-	res.write(pageFoot);
-	return res.end();
+http.createServer(async function(req, res){
+	console.log(req.url);
+	let currentURL = url.parse(req.url, true);
+	console.log('Parsituna: ' + currentURL.pathname);
+	
+	if(currentURL.pathname === '/'){
+		res.writeHead(200, {"Content-type": "text/html"});
+		//res.write('Meie veeb käivitus!');
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		// Leidsin lahenduse mis ei toiminud mul kuni ma asendasin ' märgid ära ` märkidega chatgpt abiga. Kuid me ei ole veel sellest õppinud seega ma ei kasutanud seda.
+		//res.write(`\t<p>Hetkel on ${dateTimeET.dayET()}.</p>\n\t<p>Kuupäev on: ${dateTimeET.dateET()}</p>\n\t<p>Lehekülg avati kell: ${dateTimeET.timeET()}</p>`);
+		res.write('\t<p>Hetkel on ' + dateTimeET.dayET() + '.</p>\n\t<p>Kuupäev on: ' + dateTimeET.dateET() + '</p>\n\t<p>Lehekülg avati kell: ' + dateTimeET.timeET() + '</p>');
+		res.write(pageFoot);
+		return res.end();
+	}
+	else if(currentURL.pathname === '/vanasona'){
+		res.writeHead(200, {"Content-type": "text/html"});
+		//res.write('Meie veeb käivitus!');
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write('\t<h1>Tänase päeva vanasõna</h1>\n\t<p>Siin näed tänaseks loositud Eesti vanasõna.</p>\n\t<hr>')
+		res.write(pageFoot);
+		return res.end();
+	}
+/* 	else if(currentURL.pathname === '/veebiprogrammeerimine_2026_AA.png'){
+		//liidame virtuaalse serveri päris kataloogiga
+		let bannerPath = path.join(__dirname, 'pic', currentURL.pathname);
+		fs.readFile(bannerPath, (err, data)=>{
+			if(err){
+				throw(err);
+			} else{
+				res.writeHead(200, {"Content-type": "image/png"});
+				res.end(data);
+			}
+		});
+	} */
+	else if(currentURL.pathname === '/veebiprogrammeerimine_2026_AA.png'){
+		//liidame virtuaalse serveri päris kataloogiga
+		let bannerPath = path.join(__dirname, 'pic', currentURL.pathname);
+		try{
+			const data = await fs.readFile(bannerPath);
+			res.writeHead(200, {"Content-type": "image/png"});
+			return res.end(data);
+		} catch(err){
+			res.writeHead(404, {"Content-type": "text/plain; charset=utf8"});
+			return res.end('Pilti ei leitud');
+		}
+	}
+	else{
+		res.end("Viga 404! Ei leia sellist lehte.");
+	}
 }).listen(5313);
